db0d3553319b5154dfcc2854afa734e555e21a43
Ndifreke Ndue <ndifrekeh@gmail.com>
2025-08-02 16:25:46 +0100

    Modified quiz.js

--- quiz.js
+++ quiz.js
@@ -5,7 +5,7 @@ function checkAnswer(){
     let feedback = document.getElementById('feedback');
     
     userAnswer = checked.value;
-    if(userAnswer == correctAnswer){
+    if(userAnswer === correctAnswer){
         feedback.textContent = "Correct! Well done.";
     } else {
         feedback.textContent = "That's incorrect. Try again!";
